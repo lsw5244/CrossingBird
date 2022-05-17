3c9eac9243a39e3b6645afe8e9670d871a769948
이성우 <lsw5244@naver.com>
2022-05-17 17:51:08 +0900

    feat : RoadLine구현
    RoadLine은 자동차가 주기적으로 스폰되도록 구현

---
+++ Assets/Scripts/CarMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    public float _moveSpeed = 10f;
+
+    void Update()
+    {
+        //transform.Translate(transform.forward * _moveSpeed * Time.deltaTime);
+        transform.position += transform.right * _moveSpeed * Time.deltaTime;
+    }
+}
