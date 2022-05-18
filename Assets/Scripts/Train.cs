7a393e02ed29e2026e4c7d801b861bc7b5a5d26a
이성우 <lsw5244@naver.com>
2022-05-18 17:18:45 +0900

    feat : TrainLine구현
    TrainLine은 빠른 속도로 기차가 지나간다
    기차가 지나가기 전 신호등의 상태가 변한다
    기차가 비활성화 되면 신호등의 상태가 변한다

---
+++ Assets/Scripts/Train.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    public float _moveSpeed = 40f;
+    [SerializeField]
+    private TrainLine _parentRoadLine;
+
+    void Update()
+    {
+        //transform.Translate(transform.forward * _moveSpeed * Time.deltaTime);
+        transform.position += Vector3.left * _moveSpeed * Time.deltaTime;
+    }
+
+    private void OnDisable()
+    {
+        _parentRoadLine.TurnOnGreenTrafficLight();
+    }
+}
