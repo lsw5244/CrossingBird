7ba6e83abf058355750e48a18e1fac59e2b77401
이성우 <lsw5244@naver.com>
2022-05-19 15:05:51 +0900

    feat : FlowingBoard프피렙 제작
    FlowingBoard는 활성화 될 시 1 ~ 3개 사이의 Board가 활성화 된다.

---
+++ Assets/Scripts/FlowingBoard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowingBoard : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _disableBoards;
+
+    public float _moveSpeed = 5f;
+    public int boardActivePercentage = 6;
+
+    private void Awake()
+    {
+        for(int i = 0; i < _disableBoards.Length; ++i)
+        {
+            _disableBoards[i].SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        transform.position += transform.right * _moveSpeed * Time.deltaTime;
+    }
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < _disableBoards.Length; ++i)
+        {
+            if (Random.Range(1, 11) < boardActivePercentage)
+            {
+                _disableBoards[i].SetActive(true);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < _disableBoards.Length; ++i)
+        {
+            _disableBoards[i].SetActive(false);
+        }
+    }
+}
