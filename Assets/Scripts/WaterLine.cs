829afa7ef71d2f6dd9202f8be316d05601438b98
Ojingeo <33677312+lsw5244@users.noreply.github.com>
2022-05-19 14:49:31 +0900

    feat : Water line 구현 (#3)
    * feat : 기본 WaterLine프리펩 제작
    
    * feat : WaterLine구현
    
    지정된 확률에 따라 발판이 생성되도록 구현
    
    * feat : WaterLine프리펩 화
    
    WaterLine은 지정된 확률에 따라 이동할 수 있는 발판이 생성된다.

---
+++ Assets/Scripts/WaterLine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterLine : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _boards;
+
+    public int boardActivePercentage = 3;
+
+    private void Awake()
+    {
+        for (int i = 0; i < _boards.Length; ++i)
+        {
+            _boards[i].SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        _boards[Random.Range(0, _boards.Length)].SetActive(true);
+
+        for(int i = 0; i < _boards.Length; ++i)
+        {
+            if (Random.Range(1, 11) < boardActivePercentage)
+            {
+                _boards[i].SetActive(true);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < _boards.Length; ++i)
+        {
+            _boards[i].SetActive(false);
+        }
+    }
+}
