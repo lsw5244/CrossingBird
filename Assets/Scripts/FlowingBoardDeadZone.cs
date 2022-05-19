3fafd240e1f9c7227b3ef60939e26444045f22cb
Ojingeo <33677312+lsw5244@users.noreply.github.com>
2022-05-19 19:37:34 +0900

    feat : Flowing Water Line 구현 (#4)
    * feat : FlowingBoard프피렙 제작
    
    FlowingBoard는 활성화 될 시 1 ~ 3개 사이의 Board가 활성화 된다.
    
    * feat : FlowingWaterLine구현
    
    FlowingBoard를 비활성화 시켜주는 DeadZone추가
    FlowingWaterLine은 좌, 우 중 하나의 방향을 가지고 이동하는 발판을
    주기적으로 생성함

---
+++ Assets/Scripts/FlowingBoardDeadZone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowingBoardDeadZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.parent.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/FlowingWaterLine.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowingWaterLine : MonoBehaviour
+{
+    enum BoardMoveDirection
+    {
+        Left, Right, End
+    }
+    private BoardMoveDirection _boardMoveDirection;
+
+    [SerializeField]
+    private GameObject _board;
+    private GameObject[] _boardPool;
+    private GameObject _selectBoard;
+
+    [SerializeField]
+    private int _spawnBoardCount = 2;
+
+    private Vector3 _boardSpawnPosition;
+
+    [SerializeField]
+    private float _minSpawnDelay = 1.0f;
+    [SerializeField]
+    private float _maxSpawnDelay = 3.0f;
+
+    private void Awake()
+    {
+        _boardPool = new GameObject[_spawnBoardCount];
+        for (int i = 0; i < _boardPool.Length; ++i)
+        {
+            _boardPool[i] = Instantiate(_board, _board.transform.position, Quaternion.identity);
+            _boardPool[i].transform.parent = transform;
+            _boardPool[i].SetActive(false);            
+        }
+    }
+
+    private void OnEnable()
+    {
+        _boardMoveDirection = (BoardMoveDirection)Random.Range(0, (int)BoardMoveDirection.End);
+
+        StartCoroutine(SpawnBoard());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(SpawnBoard());
+        for (int i = 0; i < _boardPool.Length; ++i)
+        {
+            _boardPool[i].SetActive(false);
+        }
+    }
+
+    IEnumerator SpawnBoard()
+    {
+        while (gameObject.activeSelf == true)
+        {
+            yield return new WaitForSeconds(Random.Range(_minSpawnDelay, _maxSpawnDelay));
+
+            _selectBoard = GetBoard();
+
+            if(_selectBoard == null)
+            {
+                continue;
+            }
+
+            SetBoardSpawnPosition();
+
+            if (_boardMoveDirection == BoardMoveDirection.Right)
+            {
+                _selectBoard.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+            else
+            {
+                _selectBoard.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+            _selectBoard.SetActive(true);
+        }
+    }
+
+    GameObject GetBoard()
+    {
+        for(int i = 0; i < _boardPool.Length; ++i)
+        {
+            if(_boardPool[i].activeSelf == false)
+            {
+                return _boardPool[i];
+            }
+        }
+
+        return null;
+    }
+
+    void SetBoardSpawnPosition()
+    {
+        _boardSpawnPosition.x = _boardMoveDirection == BoardMoveDirection.Right ? -14f : 14f;
+        _boardSpawnPosition.y = _selectBoard.transform.position.y;
+        _boardSpawnPosition.z = transform.position.z;
+        _selectBoard.transform.position = _boardSpawnPosition;
+    }
+}
