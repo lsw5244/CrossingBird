af2c3e540da5d2d4eabd7f91e3c882907bad4827
이성우 <lsw5244@naver.com>
2022-05-21 16:49:26 +0900

    feat : LineSpawnner가 Line을 Pool에 미리 저장하도록 구현

---
+++ Assets/Scripts/LineSpawnner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineSpawnner : MonoBehaviour
+{
+    enum LineType
+    {
+        FlowingWater, Forest, Road, Train, Water, End
+    }
+
+    [SerializeField]
+    private GameObject[] _lines;
+    private GameObject[,] _linePool;
+    [SerializeField]
+    private int _PoolSize = 30;
+
+    void Start()
+    {
+        //_spawnZPos = _startSpawnZPos;
+        _linePool = new GameObject[(int)LineType.End, _PoolSize];
+
+        for (int i = 0; i < _lines.Length; ++i)
+        {
+            for (int j = 0; j < _PoolSize; j++)
+            {
+                _linePool[i, j] = Instantiate(_lines[i], Vector3.zero, Quaternion.identity);
+                _linePool[i, j].SetActive(false);
+            }
+        }
+    }
+
+}
