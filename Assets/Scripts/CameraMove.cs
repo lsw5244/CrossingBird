14d71354fd9d53166541dc9cec3878ce55c34073
이성우 <lsw5244@naver.com>
2022-05-21 16:34:10 +0900

    feat : 카메라 기본 이동 구현
    카메라는 기본적으로 계속 앞으로 나아가고 플레이어의 x위치에 따라
    부드럽게 플레이어 x위치를 따라 움직인다

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    //[SerializeField]
+    //private Transform _playerTransform;
+    //public float playerFollowSpeed = 0.05f;
+
+    //public float moveSpeed = 5f;
+
+    //void Update()
+    //{
+    //    transform.position = Vector3.Lerp(transform.position, _playerTransform.position, playerFollowSpeed);
+    //}
+
     [SerializeField]
     private Transform _playerTransform;
-    public float playerFollowSpeed = 0.05f;
 
-    // Update is called once per frame
-    void Update()
+    public float playerFollowSpeed = 0.01f;
+    public float moveSpeed = 1f;
+
+    private Vector3 _nextPosition;
+
+    private void Start()
     {
-        transform.position = Vector3.Lerp(transform.position, _playerTransform.position, playerFollowSpeed);
+        _nextPosition = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        _nextPosition.x = Mathf.Lerp(transform.position.x, _playerTransform.position.x, playerFollowSpeed);
+        _nextPosition.z += moveSpeed * Time.deltaTime;
+
+        transform.position = _nextPosition;
     }
 }
